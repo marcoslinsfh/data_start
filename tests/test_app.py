7d3fdfc67a3ff0ea6ec51a142592b9176ff1073b
Marcos Luiz Lins Filho <marcoslinsfh@gmail.com>
2024-06-08 11:29:53 -0300

    feat: Refatorando o código

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from time import sleep
+import pytest
+import subprocess
 
-# Definindo o driver que vamos utilizar
-driver = webdriver.Firefox()
+@pytest.fixture
+def driver(): 
+    # Iniciar o Streamlit em backgroud
+    process = subprocess.Popen (["streamlit", "run", "src/app.py"])
+    
+    # Definindo o driver que vamos utilizar 
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    yield driver
 
-driver.set_page_load_timeout(5)
+    # Fechar o Webdriver e o Streamlit após o teste
+    driver.quit()
+    process.kill()
 
-
-try:
+def test_app_opens (driver): 
+    # Verificar se a página abriu
     driver.get("http://localhost:8501")
     sleep(5)
-    print ("Acessou a página com sucesso")
-
-except TimeoutException:
-    print ("Tempo de carregamento da página excedeu o limite")
-
-finally:
-    driver.quit()
